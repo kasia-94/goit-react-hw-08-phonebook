f9c677426323e8beecd6a9d6b7583317405f3840
Kateryna Berehovych <kasia-94@ukr.net>
2022-11-29 21:32:16 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/contactSlice';
 import { authReducer } from './auth/slice';
 import {
